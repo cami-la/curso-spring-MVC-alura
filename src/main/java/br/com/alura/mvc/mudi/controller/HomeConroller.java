69eb4a61090b8e1cbd2f98d56fc91552f357c30d
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-06-07 05:58:52 -0300

    03.04 Trabalhando com paginação

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeConroller.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeConroller.java
@@ -4,11 +4,12 @@ import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
@@ -22,9 +23,14 @@ public class HomeConroller {
 	@Autowired
 	private PedidoRepository pedidoRepository;
 	
+	
 	@GetMapping
-	public String home(Model model) {
-		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE);		
+	public String home(Model model, Principal principal) {
+		
+		Sort sort = Sort.by("dataDaEntrega").ascending();
+		PageRequest paginacao = PageRequest.of(0, 10, sort);
+		
+		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE, paginacao);		
 		model.addAttribute("pedidos", pedidos);
 
 		return "home"; // referente a página home.hml
--- src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -2,6 +2,8 @@ package br.com.alura.mvc.mudi.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,7 +15,7 @@ import br.com.alura.mvc.mudi.model.StatusPedido;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long>{
 
-	List<Pedido> findByStatus(StatusPedido status);
+	List<Pedido> findByStatus(StatusPedido status, Pageable pageable);
 
 	@Query("SELECT p FROM Pedido p JOIN p.user u WHERE u.username = :username")
 	List<Pedido> findAllByUsuario(@Param("username") String username);
